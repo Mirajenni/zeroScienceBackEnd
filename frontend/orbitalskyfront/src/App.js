a8713b5e5cdf0acc8d4bd8d10390bd71fda28264
Gabriel Amorim <gabriel.2019206500@unicap.br>
2020-10-03 00:07:00 -0300

    Subindo rotas para o Repos

--- frontend/orbitalskyfront/src/App.js
+++ frontend/orbitalskyfront/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Link to="/NewRoute">Ir para Nova Rota</Link>
         </a>
       </header>
     </div>
