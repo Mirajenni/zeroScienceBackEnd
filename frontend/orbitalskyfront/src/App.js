fab8e858e93853d3638df635fe003008ac89a2af
Gabriel Amorim <gabriel.2019206500@unicap.br>
2020-10-03 01:26:50 -0300

    FEAT: Add Sass and Login Initiate

--- frontend/orbitalskyfront/src/App.js
+++ frontend/orbitalskyfront/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Link } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- frontend/orbitalskyfront/src/index.js
+++ frontend/orbitalskyfront/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import NewRoute from "../src/NewRoute/NewRoute";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-/* Uma outra parada legal do path, é que se passarmos um Route com o path sendo um * após o último route (em nosso caso após o Route do sobre), podemos ter uma rota que representa a página 404 do nosso sistema.
+/* Uma outra parada legal do path, é que se passarmos um Route com o 
+path sendo um * após o último route (em nosso caso após o Route do sobre), 
+podemos ter uma rota que representa a página 404 do nosso sistema.
+
 <Route path='*' component={ComponenteDePagina404} /> */
 
 ReactDOM.render(
