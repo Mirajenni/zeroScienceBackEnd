80d7b996810340002012a9a468a18ed5a528d1fe
unknown <jennifer.calazans@live.com>
2020-10-03 16:33:20 -0300

    Fazendo alguns testes no projeto com n2yo

---
+++ flaskProject/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Página Login'
+
+@app.route('/main_page')
+def main():
+    return 'Página Principal da Nave'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
