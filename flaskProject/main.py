b4555ba6deace9009d50404591aa86fb612af606
vdcn1 <veetsirrah@gmail.com>
2020-10-04 23:01:37 -0300

    ADDING CORS

--- flaskProject/main.py
+++ flaskProject/main.py
@@ -1,23 +1,28 @@
 from flask import Flask
 import satelliteLocation as sl
 import json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
 
 satellite = {}
 satellite_list = [{}]
+
+app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
 
 @app.route('/')
 def index():
     return 'Página Login'
 
+@cross_origin()
 @app.route('/getSatellitesFile')
 def prepare_sat():
     fname = "test_list.txt"
     data = sl.get_all(fname, debug=True)
     return json.dumps(data)
 
+@cross_origin()
 @app.route('/getSatellitesNear')
 def prepare_near():
     data = sl.get_near(debug = True)
